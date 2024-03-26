5d60e018827b921e5d4d22641128cf86257ae56a
ty12marks <ty12marks@gmail.com>
2024-03-25 21:13:27 -0400

    New edits

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn With the Master of Power Tyler Marks!!
         </a>
       </header>
     </div>
