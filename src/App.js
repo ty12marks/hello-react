23afeee5a57d70b874ef5ef8e6afdea0be6d81a6
ty12marks <ty12marks@gmail.com>
2024-04-01 22:34:33 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
 
+const magazines = [
+  { id: 1, title: 'The New Yorker', theme: 'News', isAvailable: true},
+  { id: 2, title: 'Vogue', theme: 'Fashion', isAvailable: true},
+  { id: 3, title: 'Wired', Theme: 'design', isAvailable: True },
+];
+
+function ZineRack() {
+const ListZines = magazines.map(zinc =>
+  <li
+  key={ZineRack.id}
+  style={{
+    color: zine.isAvailable ? 'green' : 'red'
+  }}
+  >
+    {zine.title}
+    </li>
+  );
+  return (
+    <ul>{listZines}</ul>
+  )
+}
+const book = {
+  title: 'Full Metal alchemist',
+  author: 'Hiromu Arakawa',
+  published: '2001-2010',
+  image: 'https://images-na.ssl-images-amazon.com/images/I/51Q7fzj6eRL._SX331_BO1,204,203,200_.jpg',
+  width: '100',
+  height: '150'
+};
+
+function Bookshelf() {
+  return (
+    <div>
+      <h2>{book.title} ({book.published})</h2>
+      <p>{book.author}</p>
+      <img
+      className="bookcover"
+      src={book.image}
+      alt={book.title + 'cover'}
+      style={{
+        width: book.width,
+        height: book.height
+      }}
+      />
+    </div>
+  )
+}
+function MagicButton() {
+  return (
+    <> 
+      <h3>This is a Magic button</h3>
+    <button>Magic</button>
+    </>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn With the Master of Power Tyler Marks!!
-        </a>
+        <ZineRack />
+        <Bookshelf />
+        <MagicButton />
       </header>
-    </div>
+     </div>
   );
 }
 
